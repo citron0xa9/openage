976671cafd6c337ddfb5a92e16843238d3e85632
Jonas Jelten <jj@sft.mx>
2015-09-15 16:02:02 +0200

    renderer: demo id selection via cython

--- openage/testing/testlist.py
+++ openage/testing/testlist.py
@@ -46,6 +46,8 @@ def demos_py():
            "translates a C++ exception and its causes to python")
     yield ("openage.log.tests.demo",
            "demonstrates the translation of Python log messages")
+    yield ("openage.renderer.tests.renderer_demo",
+           "showcases the new renderer")
 
 
 def tests_cpp():
@@ -94,5 +96,3 @@ def demos_cpp():
            "translates a Python exception to C++")
     yield ("openage::pyinterface::tests::pyobject_demo",
            "a tiny interactive interpreter using PyObjectRef")
-    yield ("openage::renderer::tests::renderer_demo",
-           "showcases the new renderer")
